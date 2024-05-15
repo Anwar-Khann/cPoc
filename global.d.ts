d5988d21b0acc223d0e1a9818f83877cafca3bb5
Anwar-Khann <anwardev69@gmail.com>
2024-05-16 00:43:09 +0500

    "update: working"

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,11 @@
 // types/ethereum-qr-code.d.ts
-declare module 'ethereum-qr-code' {
-    export class EthereumQRPlugin {
-      toAddressString(config: object): string;
-      toCanvas(config: object, options: { selector: string }): Promise<{ value: string }>;
-      toDataUrl(config: object, options?: object): Promise<{ dataURL: string, value: string }>;
-      readStringToJSON(valueString: string): any;
-      getJSON(): string;
-      // ... other methods
-    }
-  }
+declare module'ethereum-qr-code' 
+    // export class EthereumQRPlugin {
+    //   toAddressString(config: object): string;
+    //   toCanvas(config: object, options: { selector: string }): Promise<{ value: string }>;
+    //   toDataUrl(config: object, options?: object): Promise<{ dataURL: string, value: string }>;
+    //   readStringToJSON(valueString: string): any;
+    //   getJSON(): string;
+    // }
+  
   
